d0484e4443bd1a7728d9612f22ee7fdc7a978adb
Pascal <pascale.koop@live.com>
2026-02-15 12:10:59 +0100

    Translate "Restart" button text to German as "Neustart" in ResultScreen component.

--- src/components/screens/ResultScreen.tsx
+++ src/components/screens/ResultScreen.tsx
@@ -2,7 +2,7 @@ export default function ResultScreen({ onRestart }: { onRestart: () => void }) {
   return (
     <div>
       <h2>ResultScreen</h2>
-      <button onClick={onRestart}>Restart</button>
+      <button onClick={onRestart}>Neustart</button>
     </div>
   );
 }
